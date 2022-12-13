cbdae13008ffd61b5e217e57cb47dc2be6a3cb89
lilbitzoom <katelinhogan3@gmail.com>
2022-12-13 15:05:11 -0700

    updating Javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,28 @@
-let initialCards = [];
-
-let object1 = {
+const initialCards = [
+{
   name: 'San Francisco',
   link: 'https://images.unsplash.com/photo-1670808542784-d8a43a98f35a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&q=80',
-}
-
-let object2 = {
+},
+{
   name: 'Jurassic Coast',
   link: 'https://images.unsplash.com/photo-1670793030150-75600982af60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=670&q=80',
-}
-
-let object3 ={
+},
+{
   name: 'Stavanger',
   link: 'https://images.unsplash.com/photo-1670500557067-3e2b3dde6625?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-}
-
-let object4 = {
+},
+{
   name: 'New York',
   link: 'https://images.unsplash.com/photo-1670477293988-e76875167eec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-}
-
-let object5 = {
+},
+{
   name: 'Cottonwood Canyon',
   link: 'https://images.unsplash.com/photo-1670509917257-ea50fa82f5fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80',
-}
-
-let object6 = {
+},
+{
   name: 'Base Harbor',
   link: 'https://images.unsplash.com/photo-1670349928042-519fee6e11de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=770&q=80',
 }
+];
 
-initialCards.push(object1)
-initialCards.push(object2)
-initialCards.push(object3)
-initialCards.push(object4)
-initialCards.push(object5)
-initialCards.push(object6)
-
-console.log(initialCards)
+console.log(intialCards)
