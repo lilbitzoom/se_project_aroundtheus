3c20ddead11e2a276bf0a7e846a8c44a50b159b4
lilbitzoom <katelinhogan3@gmail.com>
2022-12-29 17:30:07 -0700

    Javascript Question

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,13 +1,20 @@
-// Constant
+// Const & Let
 
 const profileContainer = document.querySelector('.profile__description');
 const profileTitle = profileContainer.querySelector('.profile__title');
-var profileName = profileTitle.querySelector('.profile__name');
-var profileSubheader = profileContainer.querySelector('.profile__subheader');
+
 const profileEditButton = profileTitle.querySelector('.profile__edit-button');
 const modal = document.querySelector('.modal');
+const modalform = modal.querySelector('.modal__container');
 const profileCloseButton = modal.querySelector('.modal__close-button');
 const profileSaveButton = modal.querySelector('.modal__save-button');
+let profileName = profileTitle.querySelector('.profile__name');
+let profileJob = profileContainer.querySelector('.profile__subheader');
+
+let inputName = modalform.querySelector('.modal__input_text_name');
+let inputDescription = modalform.querySelector(
+  '.modal__input-text_description'
+);
 
 //Open edit profile
 function addModalOpened(modal) {
@@ -28,15 +35,25 @@ profileCloseButton.addEventListener('click', function () {
 });
 
 //Edit profile title & subheader
-let title = modal.querySelector('.modal__input_text_title');
-let description = modal.querySelector('.modal__input_text_description');
-const modalContainer = modal.querySelector('.modal__container');
 
-profileName.insertAdjacentText('beforeend', '${title.value}');
-profileSubheader.insertAdjacentText('beforeend', '${description.value}');
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputDescription.value;
+  console.log('i work');
+}
 
-//Submit profile edits
-
-profileSaveButton.addEventListener('submit', function () {
-  removeModalOpened(modal);
+profileSaveButton.addEventListener('submit', function (evt) {
+  handleProfileFormSubmit(evt);
+  console.log('i works');
 });
+
+//Form reflects profile webpage
+
+function reflectNameDescription() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileJob.textContent;
+  removeModalOpened(modal);
+}
+
+reflectNameDescription();
