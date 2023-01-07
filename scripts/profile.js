7a6b44d65c31bdbc4ea9e171bae797d935ae835b
lilbitzoom <katelinhogan3@gmail.com>
2023-01-07 10:43:13 -0700

    change let to const

--- scripts/profile.js
+++ scripts/profile.js
@@ -8,11 +8,11 @@ const modal = document.querySelector('.modal');
 const profileEditForm = modal.querySelector('.modal__form');
 const profileCloseButton = modal.querySelector('.modal__close-button');
 const profileSaveButton = modal.querySelector('.modal__save-button');
-let profileName = profileTitle.querySelector('.profile__name');
-let profileJob = profileContainer.querySelector('.profile__subheader');
+const profileName = profileTitle.querySelector('.profile__name');
+const profileJob = profileContainer.querySelector('.profile__subheader');
 
-let inputName = profileEditForm.querySelector('.modal__input_text_name');
-let inputDescription = profileEditForm.querySelector(
+const inputName = profileEditForm.querySelector('.modal__input_text_name');
+const inputDescription = profileEditForm.querySelector(
   '.modal__input-text_description'
 );
 
